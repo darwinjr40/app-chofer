fd650d00c80b61513c79a235a6b87969cf001ba6
Darwin Mamani <darwin_xd10@hotmail.com>
2022-06-21 19:13:01 -0400

    darwin, register screem

--- lib/env.dart
+++ lib/env.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
 
-const primaryColor =  Colors.black;//Color(0xfff34f4f);
+const primaryColor =  Colors.blueGrey;//Color(0xfff34f4f);
 const primaryLightColor = Color(0xFFE6F0FF);
